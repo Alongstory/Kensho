04f26263bdd0a64f73e3eaebd0663e212c4c3126
Alongstory <liu.yu5@outlook.com>
2018-06-18 11:31:44 -0700

    pip1

--- pipMain.py
+++ pipMain.py
@@ -1,6 +1,12 @@
 from Crawler import Crawler
-import argparse as parser
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Process Parameters for searching')
+    parser.add_argument('--search', type = str, help = 'the search item')
+    parser.add_argument('--num', type = int, help = 'the number of returned items')
+    parser.add_argument('--key', type = str, help = 'if available, show the keys of Google API')
+    args = parser.parse_args()
+
     spider = Crawler('severe weather')
     spider.crawl()
