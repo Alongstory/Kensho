7712a05218c2ad10940126a0df6c27742d7d02e7
Alongstory <liu.yu5@outlook.com>
2018-06-19 10:43:26 -0700

    this

--- pipMain.py
+++ pipMain.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Process Parameters for searching')
     parser.add_argument('search', type = str, help = 'the search item')
     parser.add_argument('key', type=str, help='Input the keys of Google API')
-    #parser.add_argument('num', type = int, help = 'the number of returned items')
+    parser.add_argument('--num', type = int, help = 'the number of returned items')
     args = parser.parse_args()
 
-    spider = Crawler(args.search ,args.key)
+    spider = Crawler(args.search ,args.key, args.num)
     spider.crawl()
     #print(args.num)
 
